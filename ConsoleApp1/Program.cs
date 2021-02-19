2b58a26541b304ccf78c2cbc5bf569396a055b57
Sergey Lesikov <sergey_lesikov@mail.ru>
2021-02-19 21:41:49 +0300

    new snow

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -8,33 +8,79 @@ namespace ConsoleApp1
 {
     class Program
     {
+        static private char[,] snow = new char[25, 80];
+        static private Random random = new Random();
+
         static void Main(string[] args)
         {
-            string str = "  * * * * * * * * * * * *  ";
+            Console.WindowHeight = 25;
+            Console.WindowWidth = 80;
 
-            Console.WindowWidth = 25;
+            Console.Title = "SNOW";
 
-            bool toRight = true;
-
-            int pos = 0;
-
-            while (true)
+            while (!Console.KeyAvailable || Console.ReadKey().Key != ConsoleKey.Escape)
             {
+                Move();
+                GenerateFirstLineSnow();
+                Show();
+                Task.Delay(300).Wait();
+                Console.Clear();
+            }
+        }
 
-                Console.WriteLine(str.Substring(1+pos, 25));
 
-                Task.Delay(1000).Wait();
+        static void GenerateFirstLineSnow()
+        {
+            for (int i = 0; i < snow.GetLength(1); i++)
+                snow[0, i] = random.Next() % 12 <= 1 ? '*':' ';
+        }
 
-                if (toRight)
-                    pos++;
-                else
-                    pos--;
+        static void Move()
+        {
+            void ClearLine(int line)
+            {
+                for (int i = 0; i < snow.GetLength(1); i++)
+                    snow[line, i] = ' ';
+            }
 
-                if (Math.Abs(pos) > 1)
+
+            ClearLine(snow.GetLength(0)-1);
+
+            for (int i = snow.GetLength(0) - 2; i >= 0; i--)
+            {
+                for (int j = 0; j < snow.GetLength(1); j++)
                 {
-                    pos = 0;
-                    toRight = !toRight;
+                    if (snow[i, j] == '*')
+                    {
+                        int dx = j;
+
+                        if (random.Next() % 10 <= 2)
+                            dx++;
+                        else if (random.Next() % 10 <= 2)
+                            dx--;
+
+                        if (dx < 0)
+                            dx = 0;
+                        else if (dx >= snow.GetLength(1))
+                            dx = snow.GetLength(1) - 1;
+
+                        snow[i + 1, dx] = '*';
+                    }
                 }
+
+                ClearLine(i);
+            }
+        }
+
+        static void Show()
+        {
+            for (int i = 0; i < snow.GetLength(0); i++)
+            {
+                for (int j = 0; j < snow.GetLength(1); j++)
+                    Console.Write(snow[i, j]);
+
+                if(i != snow.GetLength(0)-1)
+                    Console.WriteLine();
             }
         }
     }
