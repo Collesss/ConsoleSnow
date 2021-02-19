25d39c81ffd93482c6f06bd779cc60a7109705f2
Sergey Lesikov <sergey_lesikov@mail.ru>
2021-02-19 20:25:26 +0300

    init commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "  * * * * * * * * * * * *  ";
+
+            Console.WindowWidth = 25;
+
+            bool toRight = true;
+
+            int pos = 0;
+
+            while (true)
+            {
+
+                Console.WriteLine(str.Substring(1+pos, 25));
+
+                Task.Delay(1000).Wait();
+
+                if (toRight)
+                    pos++;
+                else
+                    pos--;
+
+                if (Math.Abs(pos) > 1)
+                {
+                    pos = 0;
+                    toRight = !toRight;
+                }
+            }
+        }
+    }
+}
